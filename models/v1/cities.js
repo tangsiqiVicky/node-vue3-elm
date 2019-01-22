812a98bee59c9951e1f32222403c540745ff5af6
tangsiqiVicky <tsq282549373@163.com>
2019-01-22 16:04:45 +0800

    Update cities.js

--- models/v1/cities.js
+++ models/v1/cities.js
@@ -9,9 +9,24 @@ const citySchema = new mongoose.Schema({
 
 citySchema.statics.cityGuess = function (name) {
 	return new Promise(async (resolve, reject) => {
-		const firstWord = name.substr(0,1).toUpperCase();
-		try{
-			const city = await this.findOne()
+		const firstWord = name.substr(0, 1).toUpperCase();
+		try {
+			const city = await this.findOne();
+			Object.entries(city.data).forEach(item => {
+				if(item[0] == firstWord) {
+					item[1].forEach(cityItem => {
+						if (cityItem.pinyin == name) {
+							resolve(cityItem)
+						}
+					})
+				}
+			})
+		} catch (err) {
+			reject ({
+				name: 'ERROR_DATA',
+				message: '查找数据失败'
+			});
+			console.error(err)
 		}
 	})
 }
